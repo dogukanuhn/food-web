1bd82b6500a8293e578bf2cf286ecd451f9b8808
Berkay Doğukan Urhan <1800001773@STU.IKU.EDU.TR>
2020-09-17 22:21:41 +0300

    slick slider and custom arrow

--- components/FoodSlider/index.js
+++ components/FoodSlider/index.js
@@ -1,25 +1,60 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import Slider from 'react-slick'
 import FoodCard from '../FoodCard'
+import IconButton from '../IconButton'
+
 import styles from './foodslider.module.css'
+import cn from 'classnames'
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props
+  return (
+    <div
+      className={cn(className, styles.nextArrow, styles.arrow)}
+      style={{ ...style }}
+      onClick={onClick}
+    />
+  )
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props
+  return (
+    <div
+      className={cn(className, styles.prevArrow, styles.arrow)}
+      style={{ ...style }}
+      onClick={onClick}
+    />
+  )
+}
+
 export default function index() {
+  const settings = {
+    dots: false,
+    speed: 500,
+    slidesToShow: 4,
+    arrows: true,
+    slidesToScroll: 1,
+    className: styles.Slider,
+    draggable: false,
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SampleNextArrow />
+  }
+
   return (
     <section id={styles.foodSlider}>
       <div className="container">
-        <Row>
-          <Col xs={3}>
-            <FoodCard></FoodCard>
-          </Col>
-          <Col xs={3}>
-            <FoodCard></FoodCard>
-          </Col>
-          <Col xs={3}>
-            <FoodCard></FoodCard>
-          </Col>
-          <Col xs={3}>
-            <FoodCard></FoodCard>
-          </Col>
-        </Row>
+        <div className={styles.aboutArea}>
+          <h3>Yemeklerimiz</h3>
+        </div>
+        <Slider {...settings}>
+          <FoodCard></FoodCard>
+          <FoodCard></FoodCard>
+          <FoodCard></FoodCard>
+          <FoodCard></FoodCard>
+          <FoodCard></FoodCard>
+        </Slider>
       </div>
     </section>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/app.css'
-
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
